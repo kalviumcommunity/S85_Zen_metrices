5a3e7bf12af01011f840e7ebc4c167c97a7d84e6
sanyasimms <jain.sanya0210@gmail.com>
2025-02-25 12:00:42 +0530

    completed task

---
+++ backend/model/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+require("dotenv").config();
+mongoose.connect(process.env.MONGO_URI)
+const userSchemas = new mongoose.Schema({
+    name: String,
+    email: { type: String, unique: true },
+    age: Number,
+    weight: Number,
+    height: Number,
+    gender: String,
+    fitnessGoals: [String],
+    createdAt: { type: Date, default: Date.now }
+})
+
+const User=mongoose.model('User',userSchemas)
+module.exports=User
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,28 @@
-require("dotenv").config();
+// require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const User = require('./model/db');
 const PORT = process.env.PORT || 5000;
-
-app.use(cors());
+app.use(express.json());
+// app.use(cors());
 app.get("/ping", (req, res) => {
     res.send("pong");
 });
+
+app.post('/create',async (req,res)=>{
+    const {name,email,age,weight,height,gender,fitnessGoals} = req.body;
+    const createUser = await User.create({
+        name,
+        email,
+        age,
+        weight,
+        height,
+        gender,
+        fitnessGoals
+    });
+    res.send(createUser);
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
